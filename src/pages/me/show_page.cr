48567d837d07485cd8e3c39f4cc1f2c1d0c5c21e
Stephen Dolan <dolan.stephen1@gmail.com>
2021-01-10 18:12:08 -0500

    One more display tweak for users page

--- src/pages/me/show_page.cr
+++ src/pages/me/show_page.cr
@@ -4,7 +4,7 @@ class Me::ShowPage < MainLayout
   end
 
   def content
-    div class: "bg-white overflow-hidden shadow rounded-lg" do
+    div class: "bg-white overflow-hidden shadow sm:rounded-lg" do
       div class: "border-b border-gray-200 px-4 py-5 sm:px-6" do
         h1 "Your profile", class: "font-medium text-lg"
       end
